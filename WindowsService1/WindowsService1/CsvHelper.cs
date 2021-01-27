1065f29715fe1fd5396ed7b95044155e665de2dc
cihanbahcivan <cihanbhcvn@gmail.com>
2021-01-27 21:54:21 +0300

    Add files via upload

---
+++ WindowsService1/WindowsService1/CsvHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    public class CsvHelper
+    {
+        public void ExecuteProcess(SqlDataReader reader)
+        {
+            string path = "C:\\text.csv";
+            FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+
+            while (reader.Read())
+            {
+                string fullLine = string.Empty;
+
+
+                string belgeTarih = reader.GetDateTime(0).ToString();
+                string islemTipi = reader.GetInt32(1).ToString();
+                string vadeliTarih = reader.GetDateTime(2).ToString();
+
+                string meblag = reader.GetDouble(4).ToString();
+                meblag = meblag.Replace(',', '.');
+
+                string bakiye = reader.GetDouble(5).ToString();
+                bakiye = bakiye.Replace(',', '.');
+
+                fullLine += belgeTarih;
+                fullLine += ',';
+                fullLine += islemTipi;
+                fullLine += ',';
+                fullLine += vadeliTarih;
+                fullLine += ',';
+                fullLine += islemTipi;
+                fullLine += ',';
+                fullLine += meblag;
+                fullLine += ',';
+                fullLine += bakiye;
+
+                sw.WriteLine(fullLine);
+                sw.Flush();
+
+                reader.NextResult();
+            }
+
+            sw.Close();
+            fs.Close();
+        }
+    }
+}
---
+++ WindowsService1/WindowsService1/MyService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Configuration;
+
+namespace WindowsService1
+{
+    [RunInstaller(true)]
+    public partial class MyService : ServiceBase
+    {
+        public System.Timers.Timer timerRunTask = new System.Timers.Timer();
+        DateTime scheduledTime = DateTime.Parse(System.Configuration.ConfigurationManager.AppSettings["ScheduledTime"]);
+        public bool first = false;
+        public MyService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                timerRunTask.Interval = 30 * 1000;
+                timerRunTask.Enabled = true;
+                timerRunTask.Elapsed += new System.Timers.ElapsedEventHandler(timerRunTask_Elapsed);
+                timerRunTask.Start();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                timerRunTask.Stop();
+                timerRunTask.Dispose();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void timerRunTask_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (DateTime.Now.Hour == scheduledTime.Hour)
+            {
+                if (DateTime.Now.Minute == scheduledTime.Minute)
+                {
+                    if (!first)
+                    {
+                        GetRecord();
+                        first = true;
+                    }
+                    else
+                    {
+                        first = false;
+                    }
+
+                }
+            }
+        }
+
+        private static void GetRecord()
+        {
+            using (SqlConnection connection = new SqlConnection("Server=.;Database=TL_Cursor; Trusted_Connection=True;"))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+
+                    command.Connection = connection;
+                    command.CommandText = "exec GetCurrentExtract";
+
+                    connection.Open();
+
+                    SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                    CsvHelper helper = new CsvHelper();
+                    helper.ExecuteProcess(reader);
+
+                    connection.Close();
+                }
+            }
+
+        }
+
+    }
+}
